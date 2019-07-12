357bc2c8f6bfcda9700025db56c600be65f15ee5
DGH2112 <davidghoyle@gmail.com>
2019-07-12 14:36:22 +0100

    Updated the files to include a GNU GPL 3 declaration

--- Source/ExpertEditorForm.cpp
+++ Source/ExpertEditorForm.cpp
@@ -1,3 +1,33 @@
+/**
+  
+  This module defines a class which represents a form for the user to edit the name and location
+  of an expert.
+
+  @Author  David Hoyle
+  @Version 1.0
+  @Date    12 Jul 2019
+
+  @license
+
+    Expert Manager - a C++ Builder application for managing RAD Studio Experts (DLL and Packages)
+    in multiple version of RAD Studio.
+    
+    Copyright (C) 2019  David Hoyle (https://github.com/DGH2112/Expert-Manager)
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+**/
 #include <vcl.h>
 #pragma hdrstop
 
